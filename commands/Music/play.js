ddef4dbd7575cbc8b2042a8ed3d45f4173e9f2c3
xavierk09 <xaviervavierk@gmail.com>
2021-11-16 12:06:20 +0530

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -8,18 +8,12 @@ module.exports = {
     description: 'plays music',
     async execute(client, message, args, Discord) {
         const memberVC = message.member.voice.channel;
-        const clientVC = message.guild.me.voice.channel;
         if (!memberVC) return message.lineReplyNoMention(
             new Discord.MessageEmbed()
                 .setColor('#A9E9F6')
                 .setDescription('You need to be in a voice channel to execute this command!')
         ).then(message => { message.delete({ timeout: 10000 }); })
 
-        if (memberVC !== clientVC) return message.lineReplyNoMention(
-            new Discord.MessageEmbed()
-                .setColor('#A9E9F6')
-                .setDescription(`You need to be in ${message.guild.me.voice.channel} to execute this command!`)
-        ).then(message => { message.delete({ timeout: 10000 }); })
         const query = args.join(" ");
 
         if (!query){

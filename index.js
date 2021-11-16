df6fa5ee98739373e80e2dd0f9fb5a8d85843582
xavierk09 <xaviervavierk@gmail.com>
2021-11-16 12:58:45 +0530

    euifohaoieghaeiog

--- index.js
+++ index.js
@@ -56,10 +56,11 @@ client.distube.on("addList", async (queue, playlist) => {
 client.distube.on("initQueue", async (queue, song) => {
   queue.autoplay = false;
   queue.volume = 100;
-  const djRole = await db.fetch(`djrole.${queue.textChannel.guild.id}`)
-  if (djRole) {
-    await song.user.roles.add(djRole)
-  }
+  message.reply(queue.textChannel.guild.id)
+  // const djRole = await db.fetch(`djrole.${queue.textChannel.guild.id}`)
+  // if (djRole) {
+  //   await song.user.roles.add(djRole)
+  // }
 });
 
 client.distube.on("empty", async (queue, song) => {
@@ -70,10 +71,11 @@ client.distube.on("empty", async (queue, song) => {
       .setAuthor('Clearing queue and leaving channel!')
       .setFooter('Reason: Disconnect because voice channel is empty!')
   )
-  const djRole = await db.fetch(`djrole.${queue.textChannel.guild.id}`)
-  if (song.user.roles.cache.has(djRole)) {
-    song.user.roles.remove(djRole)
-  }
+  message.reply(queue.textChannel.guild.id)
+  // const djRole = await db.fetch(`djrole.${queue.textChannel.guild.id}`)
+  // if (song.user.roles.cache.has(djRole)) {
+  //   song.user.roles.remove(djRole)
+  // }
 }
 )
 

b0f54ee0f2791cb24ef2336684a81a96ecb16642
xavierk09 <xaviervavierk@gmail.com>
2021-11-17 12:10:01 +0530

    diguhadouighaoisgehoi

--- commands-features/Music/autoplay.js
+++ commands-features/Music/autoplay.js
@@ -19,10 +19,11 @@ module.exports = {
         )
 
         const djUser = await db.fetch(`djuser.${message.guild.id}`)
+        const djmember = await message.guild.member(djUser)
         if (message.member.id != djUser) return message.lineReply(
             new Discord.MessageEmbed()
                 .setColor('#defafe')
-                .setDescription(`you are not the dj for this music session!\n${djUser} is the current dj`)            
+                .setDescription(`you are not the dj for this music session!\n${djmember} is the current dj`)            
         )
         
         let mode = client.distube.toggleAutoplay(message);

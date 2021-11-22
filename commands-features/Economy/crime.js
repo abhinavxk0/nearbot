1f32fee0c59033c397f1d525ca892cb1a385357d
xavierk09 <xaviervavierk@gmail.com>
2021-11-22 13:14:43 +0530

    osiughaiesughiuaeg

--- commands-features/Economy/crime.js
+++ commands-features/Economy/crime.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(client, message, args, Discord){
 
         const per = Math.floor(Math.random() * 99) + 1;
-        let reward = Math.floor(Math.random() * 6000) + 2000;
+        let reward = Math.floor(Math.random() * 8000) + 2000;
         let lose = Math.floor(Math.random() * 1500) + 1000;
         let bal = await client.bal(message.author.id)
         if (bal < 1500){
@@ -22,7 +22,7 @@ module.exports = {
         
         if (winlose == true){
             client.add(message.author.id, reward)
-            message.lineReply(`you commited a crime and collected \`$${commaNumber(coins)}\` for your crime!`)
+            message.lineReply(`you commited a crime and collected \`$${commaNumber(reward)}\` for your crime!`)
         } else {
             client.del(message.author.id, lose)
             message.lineReply(`The police caught you and you were fined \`$${commaNumber(lose)}\` for your crime!`)
